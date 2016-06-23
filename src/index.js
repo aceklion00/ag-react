d6ae617de67a909e68743a9a78e2c7fdc6f88777
Ace <super.vision.dev@gmail.com>
2016-06-23 11:11:05 +0100

    release of 5.0.0-alpha.0

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 import ReactDOM from 'react-dom';
 import React from 'react';
-import MyApp from './myApp.jsx';
+import MyApp from './MyApp.jsx';
 // is there a better way of doing this?
 import 'ag-grid-root/dist/styles/ag-grid.css';
 import 'ag-grid-root/dist/styles/theme-fresh.css';
